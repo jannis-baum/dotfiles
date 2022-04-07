cfa36bb801bc4f3f178a915258aa57de1179eb3a
Jannis Baum <jannis.baum@gmail.com>
2022-04-07 16:35:06 +0200

    zsh: list alias s instead of l

--- .zsh/scripts/alias.sh
+++ .zsh/scripts/alias.sh
@@ -13,8 +13,7 @@ alias ej="eject /Volumes/LaCie"
 alias karabiner-cli="/Library/Application\\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli"
 alias convert-file="ffmpeg"
 ## list / exa
-unalias l
-alias l="COLUMNS=120 exa --all --ignore-glob='.git|node_modules|.DS_Store' --group-directories-first"
+alias s="COLUMNS=120 exa --all --ignore-glob='.git|node_modules|.DS_Store' --group-directories-first"
 unalias la
 alias la="exa --long --all --git --no-permissions --no-user --ignore-glob='.DS_Store' --group-directories-first --time-style=iso"
 alias t="exa --long --no-user --no-permissions --no-filesize --no-time --git --tree --git-ignore --all --ignore-glob='.git|node_modules|.DS_Store' --group-directories-first"
