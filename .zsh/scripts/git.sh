f8a09905d0d732d3c7bbb343051e43454593bf08
Jannis Baum <jannis.baum@gmail.com>
2022-04-21 15:12:52 +0200

    feat(zsh): ga/gr without args adds/removes all from staging

--- .zsh/scripts/git.sh
+++ .zsh/scripts/git.sh
@@ -2,10 +2,20 @@ alias gs="git status -s"
 alias gca="git commit --amend --no-edit"
 alias gal="git add -A && git status -s"
 function ga() {
-    git add $* && git status -s
+    if [[ $# -eq 0 ]]; then
+        git add --all
+    else
+        git add $*
+    fi
+    git status -s
 }
 function gr() {
-    git restore --staged $* && git status -s
+    if [[ $# -eq 0 ]]; then
+       git restore --staged $(git rev-parse --show-toplevel)
+   else
+       git restore --staged $*
+    fi
+    git status -s
 }
 function gc() {
     git commit -m "$*"
