2c9d1708439cc6ad4933c837342d92030cd7936f
Jannis Baum <jannis.baum@gmail.com>
2022-04-26 18:00:07 +0200

    git/zsh/fzf: git log features
    git/zsh/fzf: git log features
    
    git/zsh/fzf: git log features
    
    git/sh: wip git log with changes alias
    
    zsh/git/fzf: add gl to copy commit hash
    
    zsh/git/fzf: improve fzf representation
    
    zsh/git/fzf: add rebase keybinding to gl

--- .zsh/scripts/git.sh
+++ .zsh/scripts/git.sh
@@ -1,13 +1,8 @@
 alias gs="git status -s"
-alias gt="git tree"
 alias gca="git commit --amend --no-edit"
 
 function ga() {
-    if [[ $# -eq 0 ]]; then
-        git add --all
-    else
-        git add $*
-    fi
+    [[ $# -eq 0 ]] && git add --all || git add $*
     git status -s
 }
 
@@ -22,4 +17,33 @@ function gr() {
 
 function gc() {
     [[ $# -eq 0 ]] && git commit || git commit -m "$*"
+}
+
+function gco() {
+    local branch
+    branch=$(git branch -a | sed -e 's/^..//' -e '/->/d' -e 's,^remotes/origin/,,' | sort -u | fzf)
+    [[ -n $branch ]] && (git checkout $branch 2>/dev/null || git checkout -b $branch)
+}
+
+function gl() {
+    local commit hash key
+    out=$(git log --oneline --decorate --color=always \
+        | fzf --delimiter=' ' --with-nth='2..' --no-sort --exact --ansi --expect=ctrl-r)
+    key=$(echo $out | head -1)
+    hash=$(echo $out | tail -n +2 | sed 's/ .*$//')
+    if [ -n "$hash" ]; then
+        if [ "$key" = ctrl-r ]; then git rebase -i $hash^;
+        else printf $hash | pbcopy;
+        fi
+    fi
+}
+
+function glc() {
+    git log --reverse --oneline --decorate --shortstat --color=always \
+    | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n / /g' \
+    | sed -r -e 's/([0-9]+) files? changed/\x1b[34m\1: /g' \
+        -e 's/, ([0-9]+) insertions?\(\+\)/\x1b[32m+\1 /g' \
+        -e 's/, ([0-9]+) deletions?\(-\)/\x1b[31m-\1/g' \
+        -e 's/$/\x1b[0m/' \
+    | vim-ansi-pager
 }
