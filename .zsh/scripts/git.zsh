4112c7a5e5adbc15d0c2811af2d5e90fac35a1a1
Jannis Baum <jannis.baum@gmail.com>
2022-05-03 19:49:54 +0200

    zsh/git/fzf: easier git diff views

--- .zsh/scripts/git.zsh
+++ .zsh/scripts/git.zsh
@@ -38,12 +38,27 @@ function gl() {
     fi
 }
 
-function glc() {
-    git log --reverse --oneline --decorate --shortstat --color=always \
-    | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n / /g' \
-    | sed -r -e 's/([0-9]+) files? changed/\x1b[34m\1: /g' \
-        -e 's/, ([0-9]+) insertions?\(\+\)/\x1b[32m+\1 /g' \
-        -e 's/, ([0-9]+) deletions?\(-\)/\x1b[31m-\1/g' \
-        -e 's/$/\x1b[0m/' \
-    | vim-ansi-pager
+function gd() {
+    local file
+    file=$(git diff --stat=120 --color=always $1 \
+        | sed '$d' \
+        | fzf --ansi --exit-0 \
+        | sed -r 's/^ *([^[:blank:]]*) *\|.*$/\1/')
+    [[ -n "$file" ]] && git difftool $1 "$file"
 }
+function _MINE_git_branch_names() {
+    compadd "${(@)${(f)$(git branch -a)}#??}"
+}
+compdef _MINE_git_branch_names gd
+
+# shows number of line changes for each commit, almost never useful
+# but want to keep it somewhere
+#function glc() {
+#    git log --reverse --oneline --decorate --shortstat --color=always \
+#    | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n / /g' \
+#    | sed -r -e 's/([0-9]+) files? changed/\x1b[34m\1: /g' \
+#        -e 's/, ([0-9]+) insertions?\(\+\)/\x1b[32m+\1 /g' \
+#        -e 's/, ([0-9]+) deletions?\(-\)/\x1b[31m-\1/g' \
+#        -e 's/$/\x1b[0m/' \
+#    | vim-ansi-pager
+#}
