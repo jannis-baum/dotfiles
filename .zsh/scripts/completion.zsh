a290921dce2792bd887411b95aa4843c5d6da508
Jannis Baum <jannis.baum@gmail.com>
2022-05-03 19:50:48 +0200

    zsh/completion: complete on approximate matches

--- .zsh/scripts/completion.zsh
+++ .zsh/scripts/completion.zsh
@@ -1,7 +1,5 @@
 # hidden files & dirs
 setopt globdots
-# typos
-setopt correct_all
 
 autoload -U compinit; compinit
 
@@ -12,6 +10,8 @@ zstyle ':completion:*' menu select
 
 # case & hyphen insensitive, substrings
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# normal and approximate (typo) matches
+zstyle ':completion:*' completer _extensions _complete _approximate
 
 # cache
 zstyle ':completion:*' use-cache on
