14bf1fff1443e689b0c11ee8dcf5da542b12c66c
Jannis Baum <jannis.baum@gmail.com>
2022-05-05 17:04:25 +0200

    zsh/git/fzf: have gl use gd

--- .zsh/scripts/git.zsh
+++ .zsh/scripts/git.zsh
@@ -27,27 +27,35 @@ function gco() {
     [[ -n $branch ]] && (git checkout $branch 2>/dev/null || git checkout -b $branch)
 }
 
+function _git_pretty_diff() {
+    paste -d '\0' \
+        <(git diff --name-status $1 $2 | sed -r 's/^([^[:blank:]]).*$/\1/') \
+        <(git diff --stat=118 --color=always $1 $2)
+}
+
 function gl() {
     local commit hash key
     out=$(git log --oneline --decorate --color=always \
-        | fzf --delimiter=' ' --with-nth='2..' --no-sort --exact --ansi --expect=ctrl-r)
+        | fzf --delimiter=' ' --with-nth='2..' --no-sort --exact --ansi --expect=ctrl-r,ctrl-o \
+            --preview 'zsh -c "source $ZDOTDIR/scripts/git.zsh 2> /dev/null;
+                _git_pretty_diff $(git log --pretty=%P -n 1 {1}) {1} | less -R"' \
+            --preview-window='50%,nowrap,nohidden')
     key=$(echo $out | head -1)
     hash=$(echo $out | tail -n +2 | sed 's/ .*$//')
     if [ -n "$hash" ]; then
         if [[ "$key" == ctrl-r ]]; then git rebase -i $hash^;
-        else printf $hash | pbcopy;
+        elif [[ "$key" == ctrl-o ]]; then printf $hash | pbcopy;
+        else gd $(git log --pretty=%P -n 1 $hash) $hash; gl;
         fi
     fi
 }
 
 function gd() {
     local file
-    file=$(paste -d '\0' \
-        <(git diff --name-status $1 | sed -r 's/^([^[:blank:]]).*$/\1/') \
-        <(git diff --stat=118 --color=always $1 | sed '$d') \
-        | fzf --ansi --exit-0 \
+    file=$(_git_pretty_diff $1 $2 | sed '$d' \
+        | fzf --ansi --exit-0 --preview=''\
         | sed -r 's/^. *([^[:blank:]]*) *\|.*$/\1/')
-    [[ -n "$file" ]] && git difftool $1 "$(git rev-parse --show-toplevel)/$file" && gd $1
+    [[ -n "$file" ]] && git difftool $1 $2 "$(git rev-parse --show-toplevel)/$file" && gd $1 $2
 }
 function _MINE_git_branch_names() {
     compadd "${(@)${(f)$(git branch -a)}#??}"
