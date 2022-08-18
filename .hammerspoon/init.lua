d6e5bdd27ed7acb1e67a6139682bb3c3d8c98ecf
Jannis Baum <jannis.baum@gmail.com>
2022-08-18 15:52:52 +0200

    hammerspoon: listen to kV mode changes

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,1 +1,15 @@
+-- launch agent
+
 hs.application.launchOrFocus('Synapse')
+
+-- kindaVim notifications
+
+kVNormalEnterWatcher = hs.distributednotifications.new(function(name, object, userInfo)
+    hs.execute('/Library/Application\\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --set-variables "{\\"kVNormal\\":1}"')
+end, 'kindaVimDidEnterNormalMode')
+kVNormalEnterWatcher:start()
+
+kVNormalExitWatcher = hs.distributednotifications.new(function(name, object, userInfo)
+    hs.execute('/Library/Application\\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --set-variables "{\\"kVNormal\\":0}"')
+end, 'kindaVimDidExitNormalMode')
+kVNormalExitWatcher:start()
