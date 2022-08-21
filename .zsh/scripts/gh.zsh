9e5bcac8528abbfa44f495e782ea2faf08571316
Jannis Baum <jannis.baum@gmail.com>
2022-08-21 11:43:09 +0200

    gh/zsh: add github issue command

---
+++ .zsh/scripts/gh.zsh
@@ -0,0 +1,28 @@
+function gi() {
+    local out key issue
+
+    out=$(GH_FORCE_TTY='45%' gh issue list \
+        | fzf --ansi --header-lines 3 --expect=ctrl-b\
+            --preview-window='50%,nowrap,nohidden' \
+            --preview 'GH_FORCE_TTY=$FZF_PREVIEW_COLUMNS gh issue view {1}')
+
+    key=$(echo $out | head -1)
+    issue=$(echo $out | tail -n +2 | sed -r 's/^#([[:digit:]]+) .*/\1/')
+
+    if [ -n "$issue" ]; then
+        if [[ "$key" == ctrl-b ]]; then
+            local branch
+            branch="issue/$issue-$(gh issue view $issue \
+                | rg 'title:' \
+                | sed -r 's/^title:[[:blank:]]*//' \
+                | tr ' ' '-' \
+                | tr -cd '[:alnum:]-' \
+                | tr '[:upper:]' '[:lower:]' \
+                | sed -r 's/--+/-/g')"
+            git checkout -b $branch 2>/dev/null \
+                || git checkout $branch
+        else
+            gh issue view --web $issue
+        fi
+    fi
+}
