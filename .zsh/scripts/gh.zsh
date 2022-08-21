ceaed28757d1192f775fcede299e950198de6a5e
Jannis Baum <jannis.baum@gmail.com>
2022-08-21 11:48:12 +0200

    gh/zsh: add ctrl-o to copy issue number

--- .zsh/scripts/gh.zsh
+++ .zsh/scripts/gh.zsh
@@ -2,7 +2,7 @@ function gi() {
     local out key issue
 
     out=$(GH_FORCE_TTY='45%' gh issue list \
-        | fzf --ansi --header-lines 3 --expect=ctrl-b\
+        | fzf --ansi --header-lines 3 --expect=ctrl-b,ctrl-o\
             --preview-window='50%,nowrap,nohidden' \
             --preview 'GH_FORCE_TTY=$FZF_PREVIEW_COLUMNS gh issue view {1}')
 
@@ -10,7 +10,9 @@ function gi() {
     issue=$(echo $out | tail -n +2 | sed -r 's/^#([[:digit:]]+) .*/\1/')
 
     if [ -n "$issue" ]; then
-        if [[ "$key" == ctrl-b ]]; then
+        if [[ "$key" == ctrl-o ]]; then
+            printf "#$issue" | pbcopy
+        elif [[ "$key" == ctrl-b ]]; then
             local branch
             branch="issue/$issue-$(gh issue view $issue \
                 | rg 'title:' \
