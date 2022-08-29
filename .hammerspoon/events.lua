9b687ca743d71184598900336c01dbd94e4f0d51
Jannis Baum <jannis.baum@gmail.com>
2022-08-29 08:53:04 +0200

    hammerspoon: split into files

---
+++ .hammerspoon/events.lua
@@ -0,0 +1,15 @@
+-- launch
+hs.execute('open -g -a Synapse')
+
+-- reload config
+hs.urlevent.bind("reloadConfig", function(eventName, params)
+    hs.reload()
+end)
+
+-- sleep
+sleepWatcher = hs.caffeinate.watcher.new(function(event)
+    if event == hs.caffeinate.watcher.systemWillSleep then
+        hs.execute('~/_lib/cookie-cleaner/.build/release/cookie-cleaner ~/_lib/cookie-cleaner/whitelist.txt')
+    end
+end)
+sleepWatcher:start()
---
+++ .hammerspoon/kvim.lua
@@ -0,0 +1,9 @@
+kVNormalEnterWatcher = hs.distributednotifications.new(function(name, object, userInfo)
+    hs.execute('/Library/Application\\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --set-variables "{\\"kVNormal\\":1}"')
+end, 'kindaVimDidEnterNormalMode')
+kVNormalEnterWatcher:start()
+
+kVNormalExitWatcher = hs.distributednotifications.new(function(name, object, userInfo)
+    hs.execute('/Library/Application\\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --set-variables "{\\"kVNormal\\":0}"')
+end, 'kindaVimDidExitNormalMode')
+kVNormalExitWatcher:start()
