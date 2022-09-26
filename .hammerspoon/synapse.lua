d69c28a0fc0c6cafba8694bfaf5c6102e1f392f8
Jannis Baum <jannis.baum@gmail.com>
2022-09-26 16:45:19 +0200

    synapse: start control center module

--- .hammerspoon/synapse.lua
+++ .hammerspoon/synapse.lua
@@ -1,2 +1,12 @@
 -- launch
 hs.execute('open -g -a Synapse')
+
+-- system key sink
+hs.urlevent.bind("systemKey", function(eventName, params)
+    local key = params['key']
+    if key == nil then
+        return
+    end
+    hs.eventtap.event.newSystemKeyEvent(key, true):post()
+    hs.eventtap.event.newSystemKeyEvent(key, false):post()
+end)
