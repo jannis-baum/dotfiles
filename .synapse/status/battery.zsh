6f1088cc6d5f03495ba2eb39a076d97a426c751e
Jannis Baum <jannis.baum@gmail.com>
2022-10-08 15:31:15 +0200

    synapse: battery script & move to placeholder

---
+++ .synapse/status/battery.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+BATTERY=$(pmset -g batt)
+
+[[ -z "$BATTERY" || "$BATTERY" == *charged* ]] && exit 0
+[[ "$BATTERY" == *discharging* ]] || printf '⚡'
+
+if [[ "$1" == "--percentage" || "$1" == "-p" ]]; then
+    echo "$BATTERY" | grep -o '\d\+%'
+else
+    echo "$BATTERY" | grep -o '\d\+:\d\+' || echo '⋯'
+fi
