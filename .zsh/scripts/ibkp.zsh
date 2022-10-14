07444625d69a9570daf939b68cc4c824b5ba9b6e
Jannis Baum <jannis.baum@gmail.com>
2022-10-14 20:28:17 +0200

    zsh/ibkp: add some size reading features

--- .zsh/scripts/ibkp.zsh
+++ .zsh/scripts/ibkp.zsh
@@ -3,10 +3,12 @@ ibkp() {
     mkdir -p $backup_dir
 
     for arg in "$@"; do
-        if [[ "$arg" == "-d" || "$arg" == "--cd" ]] then;
-            local should_cd=1
-            continue
-        fi
+        case "$arg" in
+            "-p" | "--print-directory") local should_print=1; continue;;
+            "-t" | "--tree-size") local should_tree=1; continue;;
+            "-s" | "--size") local should_size=1; continue;;
+            "-d" | "--cd") local should_cd=1; continue;;
+        esac
 
         local dir_name=$(dirname $arg)
         local source_dir=$(realpath $dir_name)
@@ -14,11 +16,23 @@ ibkp() {
             echo "\e[2m\e[3m$arg not in home directory, skipping\e[0m"
             continue
         fi
+
+        if [[ -n "$should_size" ]]; then
+            du -sh $arg
+        fi
+
         local dest_dir=$(sed "s|$HOME|$backup_dir|" <<< "$source_dir")
         local file_name=$(sed "s|$dir_name||" <<< "$arg")
         mkdir -p "$dest_dir"
         cp -r "$arg" "$dest_dir$file_name"
     done
 
+    [[ -n "$should_print" ]] && echo $backup_dir
+    [[ -n "$should_tree" ]] && \
+        exa --long --no-user --no-permissions --no-time \
+            --tree --all --group-directories-first \
+            --ignore-glob='.git|node_modules|.DS_Store' \
+            $backup_dir
+    [[ -n "$should_size" ]] && du -sh $backup_dir | sed -r "s|$backup_dir/?|total backups size|"
     [[ -n "$should_cd" ]] && cd $backup_dir
 }
