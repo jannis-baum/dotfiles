25de0482d0e18d979c40ce36b94ae4252b384821
Jannis Baum <jannis.baum@gmail.com>
2022-10-14 20:52:57 +0200

    synapse/hammerspoon: generalize control center sink

--- .hammerspoon/synapse.lua
+++ .hammerspoon/synapse.lua
@@ -1,12 +1,21 @@
 -- launch
 hs.execute('open -g -a Synapse')
 
--- system key sink
-hs.urlevent.bind("systemKey", function(eventName, params)
-    local key = params['key']
-    if key == nil then
+-- control center sink
+local fnLookup = {
+    ['systemKey'] = function(key)
+        hs.eventtap.event.newSystemKeyEvent(key, true):post()
+        hs.eventtap.event.newSystemKeyEvent(key, false):post()
+    end
+}
+hs.urlevent.bind("controlCenter", function(eventName, params)
+    local info = params['info']
+    if info == nil then
         return
     end
-    hs.eventtap.event.newSystemKeyEvent(key, true):post()
-    hs.eventtap.event.newSystemKeyEvent(key, false):post()
+
+    for k, v in string.gmatch(info, "([%w_-]+):([%w_-]+)") do
+        local fn = fnLookup[k]
+        if (fn) then fn(v) end
+     end
 end)
