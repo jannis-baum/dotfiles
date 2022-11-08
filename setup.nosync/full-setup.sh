37d616bc3083263bc7ee21c1eed7631a7758c7ef
Jannis Baum <jannis.baum@gmail.com>
2022-11-08 18:20:33 +0100

    setup: option to specify dotfiles install dir

--- setup.nosync/full-setup.sh
+++ setup.nosync/full-setup.sh
@@ -4,24 +4,23 @@ function hl_print() {
     printf "\\033[1m$1\\033[0m\n"
 }
 
+[[ -n "$DOTFILES_DIR" ]] && DOTFILES_DIR=$HOME/_dotfiles
+
+hl_print "CLONING INTO DOTFILES & INITIALIZING SUBMODULES"
+printf "\\033[2m"
+git clone --recurse-submodules https://github.com/jannis-baum/dotfiles $DOTFILES_DIR
+printf "\\033[0m"
+
+hl_print "SETTING MACOS PREFERENCES"
+$DOTFILES_DIR/setup/macos.sh
+
 if ! which brew &> /dev/null; then
     hl_print "INSTALLING HOMEBREW"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
-
-dotfiles_dir=$HOME/_dotfiles
-
-hl_print "CLONING REPO & INITIALIZING SUBMODULES"
-printf "\\033[2m"
-git clone --recurse-submodules https://github.com/jannis-baum/dotfiles $dotfiles_dir
-printf "\\033[0m"
-
-hl_print "SETTING MACOS PREFERENCES"
-$dotfiles_dir/setup/macos.sh
-
 hl_print "INSTALLING BREW PACKAGES"
-$dotfiles_dir/setup/brew.sh --upgrade
+$DOTFILES_DIR/setup/brew.sh --upgrade
 
 hl_print "INSTALLING CONFIG"
-source $dotfiles_dir/../.zsh/scripts/sdf.zsh
+source $DOTFILES_DIR/../.zsh/scripts/sdf.zsh
 jot -s '' -b y 0 | sdf
