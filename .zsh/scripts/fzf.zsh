b75d0eeb8d75d1c45df025bf5d49f3f67e76ca63
Jannis Baum <jannis.baum@gmail.com>
2022-12-16 16:02:22 +0100

    zsh/fzf: fix ctrl-u ctrl-n path

--- .zsh/scripts/fzf.zsh
+++ .zsh/scripts/fzf.zsh
@@ -55,7 +55,7 @@ fzf_dir() {
     dir=$(tail -n +2 <<< $out)
     if [[ -n "$dir" ]]; then
         if [[ -n "$BUFFER" || "$key" == ctrl-o ]]; then LBUFFER+=${(q-)dir};
-        elif [[ "$key" == ctrl-n ]]; then LBUFFER="v ${(q-)dir}/";
+        elif [[ "$key" == ctrl-n ]]; then LBUFFER="v ${(q-)dir}";
         elif [[ "$key" == ctrl-u ]]; then LBUFFER="mkdir -p ${(q-)dir}";
         else BUFFER="cd ${(q-)dir}"; zle accept-line; zle reset-prompt;
         fi
