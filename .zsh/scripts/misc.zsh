20e53dd586e5d864e95b74e6ddd626ea29f74d19
Jannis Baum <jannis.baum@gmail.com>
2023-01-12 14:01:29 +0100

    zsh: improve readability of passn

--- .zsh/scripts/misc.zsh
+++ .zsh/scripts/misc.zsh
@@ -18,10 +18,15 @@ function v() {
     mkdir -p $(dirname $1) && $EDITOR $1
 }
 ## generate new password
-function pass-n() {
+function passn() {
 	echo | pbcopy
 	while [[ $(pbpaste) =~ '^[^0-9]*$' || $(pbpaste) =~ '^[^a-z]*$' || $(pbpaste) =~ '^[^A-Z]*$' ]]; do
-        cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9-_\$,.!?/' | fold -w 32 | sed 1q | tr -d '\n' | pbcopy
+        cat /dev/urandom \
+            | LC_ALL=C tr -dc 'a-zA-Z0-9-_\$,.!?:;~`^+=@&%#*[](){}/' \
+            | fold -w 32 \
+            | head -1 \
+            | tr -d '\n' \
+            | pbcopy
 	done
 }
 ## quick look
