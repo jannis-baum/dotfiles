a7f8940291ea2df092d65cf36d691796e755c950
Jannis Baum <jannis.baum@gmail.com>
2023-04-21 14:43:13 +0200

    zsh: remove si vim predecessor

--- .zsh/scripts/keybindings.zsh
+++ .zsh/scripts/keybindings.zsh
@@ -11,11 +11,3 @@ bindkey -M vicmd j down-line-or-beginning-search
 # shift+tab to go back in completion
 bindkey -M viins "${terminfo[kcbt]}" reverse-menu-complete
 bindkey -M vicmd "${terminfo[kcbt]}" reverse-menu-complete
-
-# ctrl-u for fg
-_fg_widget() {
-    BUFFER="fg"
-    zle accept-line; zle reset-prompt
-}
-zle -N _fg_widget
-bindkey ^u _fg_widget
--- .zsh/scripts/si_vim.zsh
+++ .zsh/scripts/si_vim.zsh
@@ -1,33 +1,36 @@
-function _get_vim_ids() {
-    jobs | sed -E -n 's/\[([0-9])\][ +-]* [a-zA-Z]*[[:space:]]*v .*/\1/p'
+# single instance vim - si_vim
+# always keeps one instance of vim running in the background ready to be used
+# for anything
+
+# function to find job name in list
+function _si_vim_job() {
+    vim
 }
+# check if si_vim is running
+function _si_vim_isrunning() {
+    [[ -n "$(jobs | grep '_si_vim_job')" ]]
+}
+
+# ensure si_vim is always running
+autoload -U add-zsh-hook
+function _si_vim_run() {
+    _si_vim_isrunning || _si_vim_job &
+}
+add-zsh-hook precmd _si_vim_run
 
 # make dirs to open editor
 # open in running vim if suspended
 function v() {
     mkdir -p $(dirname $1)
 
-    local vim_id=$(_get_vim_ids | head -1)
-    if [[ -n "$vim_id" ]]; then
-        echo "tabedit $1\nfiletype detect" > $HOME/.vim/resume-source.vim
-        fg %$vim_id
-    else
-        vim $1
-    fi
+    echo "tabedit $1\nfiletype detect" > $HOME/.vim/resume-source.vim
+    fg %_si_vim_job
 }
 
-# function to find job name in list
-function __si_vim() {
-    vim
+# ctrl-u to bring up si_vim
+_si_vim_widget() {
+    BUFFER="fg %_si_vim_job"
+    zle accept-line; zle reset-prompt
 }
-# check if si_vim is running
-function _si_vim_isrunning() {
-    [[ -n "$(jobs | grep '__si_vim')" ]]
-}
-
-# ensure si_vim is always running
-autoload -U add-zsh-hook
-function _si_vim_run() {
-    _si_vim_isrunning || __si_vim &
-}
-add-zsh-hook precmd _si_vim_run
+zle -N _si_vim_widget
+bindkey ^u _si_vim_widget
