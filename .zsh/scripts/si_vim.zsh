951ff4404c3394616902f087ccdeabca6c0e18e1
Jannis Baum <jannis.baum@gmail.com>
2023-04-21 15:45:31 +0200

    zsh: keep si_vim dir in sync

--- .zsh/scripts/si_vim.zsh
+++ .zsh/scripts/si_vim.zsh
@@ -6,6 +6,10 @@
 _si_vim_resume_source_dir=$HOME/.vim/resume-source
 mkdir -p $_si_vim_resume_source_dir
 _si_vim_resume_source=$_si_vim_resume_source_dir/$$.vim
+# add to command buffer
+function _si_vim_cmd() {
+    echo "$1" >> $_si_vim_resume_source
+}
 
 # function to find job name in list
 function _si_vim_job() {
@@ -16,19 +20,25 @@ function _si_vim_isrunning() {
     [[ -n "$(jobs | grep '_si_vim_job')" ]]
 }
 
-# ensure si_vim is always running
 autoload -U add-zsh-hook
+
+# ensure si_vim is always running
 function _si_vim_run() {
     _si_vim_isrunning || _si_vim_job &
 }
 add-zsh-hook precmd _si_vim_run
 
+# keep si_vim in same directory as zsh
+function _si_vim_syncpwd() {
+    _si_vim_cmd "cd $(pwd)"
+}
+add-zsh-hook chpwd _si_vim_syncpwd
+
 # make dirs to open editor
 # open in running vim if suspended
 function v() {
     mkdir -p $(dirname $1)
-
-    echo "SivOpen $1" > $_si_vim_resume_source
+    _si_vim_cmd "SivOpen $1"
     fg %_si_vim_job
 }
 
