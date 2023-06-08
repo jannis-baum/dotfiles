6b6551dae820c22326d4060ff2fcb1ea3a178ae4
Jannis Baum <jannis.baum@gmail.com>
2023-06-08 11:50:33 +0200

    hammerspoon: run cookie-cleaner when safari quits

--- .hammerspoon/events.lua
+++ .hammerspoon/events.lua
@@ -3,10 +3,10 @@ hs.urlevent.bind("reloadConfig", function(eventName, params)
     hs.reload()
 end)
 
--- sleep
-sleepWatcher = hs.caffeinate.watcher.new(function(event)
-    if event == hs.caffeinate.watcher.systemWillSleep then
-        hs.execute('~/_lib/cookie-cleaner/.build/release/cookie-cleaner ~/.safari/cookie-whitelist.txt')
+-- apps
+safariWatcher = hs.application.watcher.new(function(name, eventType, app)
+    if name == 'Safari' and eventType == hs.application.watcher.terminated then
+        out, status = hs.execute('~/_lib/cookie-cleaner/.build/release/cookie-cleaner ~/.safari/cookie-whitelist.txt')
     end
 end)
-sleepWatcher:start()
+safariWatcher:start()
