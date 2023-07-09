ceddc490ca24d6d37d20117a5e72180fa52d37b9
Jannis Baum <jannis.baum@gmail.com>
2023-07-09 13:29:32 +0200

    zsh: add custom si-vim binding

--- .zsh/scripts/keybindings.zsh
+++ .zsh/scripts/keybindings.zsh
@@ -11,3 +11,6 @@ bindkey -M vicmd j down-line-or-beginning-search
 # shift+tab to go back in completion
 bindkey -M viins "${terminfo[kcbt]}" reverse-menu-complete
 bindkey -M vicmd "${terminfo[kcbt]}" reverse-menu-complete
+
+# si-vim
+bindkey ^u _si_vim_widget
