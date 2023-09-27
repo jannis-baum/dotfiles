59271c7162a06e4b75b6a2c0b69e5ddb1718373b
Jannis Baum <jannis.baum@gmail.com>
2023-09-27 21:46:03 +0200

    zsh: g for gsi

--- .zsh/scripts/misc.zsh
+++ .zsh/scripts/misc.zsh
@@ -1,10 +1,10 @@
 # generic aliases
 alias c="printf '\n%.0s' {2..$LINES} && clear"
 alias ej="diskutil eject /Volumes/LaCie*"
-alias convert-file="ffmpeg"
 alias w='printf "%s" "$(VISUAL=vim vipe)" | pbcopy'
 alias ssh='kitty +kitten ssh'
 alias trm='trash -F'
+alias g='gsi'
 
 # linking scripts
 alias mue="~/.lib/markup-export/main"
