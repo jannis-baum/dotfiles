bfa6c939b0e3cdd5ac57f8491d500c18f6783d7a
Jannis Baum <jannis.baum@gmail.com>
2024-04-20 09:36:51 +0200

    zsh: create new fbm

--- .zsh/scripts/fbm.zsh
+++ .zsh/scripts/fbm.zsh
@@ -6,3 +6,13 @@ function _fbm_find() {
     done
     )
 }
+
+function fbm() {
+    if [[ "$1" == "-n" || "$1" == "--new" ]]; then
+        if [[ "$#" != "3" ]]; then
+            echo "usage: fbm [-n | --new] name uri"
+            return 1
+        fi
+        echo "$2\t$3" >> .fbms
+    fi
+}
