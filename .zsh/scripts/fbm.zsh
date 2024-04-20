38eea09905be9c1a6e527c1d0ef09fff4a545522
Jannis Baum <jannis.baum@gmail.com>
2024-04-20 13:30:53 +0200

    zsh: fbm list with source path

--- .zsh/scripts/fbm.zsh
+++ .zsh/scripts/fbm.zsh
@@ -1,3 +1,6 @@
+_fbm_path_color=$(sed -r 's/^.*di=([^:]+):.*$/\1/' <<< $LS_COLORS)
+[[ -n "$_fbm_path_color" ]] && _fbm_path_color="\\x1b[${_fbm_path_color}m"
+
 function fbm() {
     if [[ "$1" == "-n" || "$1" == "--new" ]]; then
         if [[ "$#" != "3" ]]; then
@@ -9,11 +12,14 @@ function fbm() {
     fi
 
     local target=$((
+        local prefix="."
         while [[ "$(pwd)" != "/" ]]; do
-            test -f '.fbms' && cat .fbms
+            local path_comp="$_fbm_path_color$prefix/$(basename "$(realpath .)")/\\x1b[0m"
+            test -f '.fbms' && cat .fbms | sed -e "s|^|$path_comp|"
             cd ..
+            prefix="$prefix."
         done
-    ) | fzf --delimiter="\t" --with-nth=1)
+    ) | fzf --delimiter="\t" --with-nth=1 --ansi)
 
     [[ -z "$target" ]] && return
     open "$(cut -f2 <<< "$target")"
