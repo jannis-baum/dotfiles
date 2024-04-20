21853c5c5f4cddc1bdce5f6fcb7fc918cb7d29d9
Jannis Baum <jannis.baum@gmail.com>
2024-04-20 09:44:00 +0200

    zsh: finish fbms

--- .zsh/scripts/fbm.zsh
+++ .zsh/scripts/fbm.zsh
@@ -1,12 +1,3 @@
-function _fbm_find() {
-    (
-    while [[ "$(pwd)" != "/" ]]; do
-        test -f '.fbms' && cat .fbms
-        cd ..
-    done
-    )
-}
-
 function fbm() {
     if [[ "$1" == "-n" || "$1" == "--new" ]]; then
         if [[ "$#" != "3" ]]; then
@@ -14,5 +5,16 @@ function fbm() {
             return 1
         fi
         echo "$2\t$3" >> .fbms
+        return
     fi
+
+    local target=$((
+        while [[ "$(pwd)" != "/" ]]; do
+            test -f '.fbms' && cat .fbms
+            cd ..
+        done
+    ) | fzf --delimiter="\t" --with-nth=1)
+
+    [[ -z "$target" ]] && return
+    open "$(cut -f2 <<< "$target")"
 }
