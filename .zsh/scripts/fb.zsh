430bbe22fc4c574616367b71ef1d3768ebf1e42b
Jannis Baum <jannis.baum@gmail.com>
2024-04-20 13:42:53 +0200

    zsh/git: rename fbms to fbs

--- .zsh/scripts/fb.zsh
+++ .zsh/scripts/fb.zsh
@@ -1,13 +1,13 @@
-_fbm_path_color=$(sed -r 's/^.*di=([^:]+):.*$/\1/' <<< $LS_COLORS)
-[[ -n "$_fbm_path_color" ]] && _fbm_path_color="\\x1b[${_fbm_path_color}m"
+_fb_path_color=$(sed -r 's/^.*di=([^:]+):.*$/\1/' <<< $LS_COLORS)
+[[ -n "$_fb_path_color" ]] && _fb_path_color="\\x1b[${_fb_path_color}m"
 
-function fbm() {
+function fb() {
     if [[ "$1" == "-n" || "$1" == "--new" ]]; then
         if [[ "$#" != "3" ]]; then
-            echo "usage: fbm [-n | --new] name uri"
+            echo "usage: fb [-n | --new] name uri"
             return 1
         fi
-        echo "$2\t$3" >> .fbms
+        echo "$2\t$3" >> .fbs
         return
     fi
 
@@ -15,8 +15,8 @@ function fbm() {
         local prefix="."
         while [[ "$(pwd)" != "/" ]]; do
             local dir="$(realpath .)"
-            local path_comp="$_fbm_path_color$prefix/$(basename "$dir")/\\x1b[0m"
-            test -f '.fbms' && cat .fbms | sed -e "s|^|$dir\t$path_comp|"
+            local path_comp="$_fb_path_color$prefix/$(basename "$dir")/\\x1b[0m"
+            test -f '.fbs' && cat .fbs | sed -e "s|^|$dir\t$path_comp|"
             cd ..
             prefix="$prefix."
         done
@@ -31,7 +31,7 @@ function fbm() {
     [[ -z "$pick" ]] && return
 
     if [[ "$key" == "ctrl-o" ]]; then
-        $EDITOR "$(cut -f1 <<< "$pick")/.fbms"
+        $EDITOR "$(cut -f1 <<< "$pick")/.fbs"
     else
         open "$(cut -f3 <<< "$pick")"
     fi
