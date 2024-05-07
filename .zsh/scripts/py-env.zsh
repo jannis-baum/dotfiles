0d22afefefd7fdb0435b785fda95ef6ca33e4b1e
Jannis Baum <jannis.baum@gmail.com>
2024-05-07 19:58:51 +0200

    zsh: auto-pyv

--- .zsh/scripts/py-env.zsh
+++ .zsh/scripts/py-env.zsh
@@ -1,10 +1,7 @@
 function pyv() {
-    local pyv_path="$HOME/.pyv"
-    mkdir -p $pyv_path
-
     # MARK: ARUGMENT PARSING ---------------------------------------------------
     # from https://gist.github.com/jannis-baum/d3e5744466057f4e61614744a2397fdd
-    local arg_help="" positional=() arg_list arg_new arg_noipython
+    local arg_help="" positional=() arg_location arg_new arg_noipython
 
     while (( $# )); do
         _echo_error() {
@@ -15,43 +12,46 @@ function pyv() {
 
         case "$arg" in
             "-h" | "--help") arg_help=1; continue;;
-            "-l" | "--list") arg_list=1; continue;;
+            "-l" | "--location") arg_location=1; continue;;
             "-p" | "--no-ipython") arg_noipython=1; continue;;
             "-n" | "--new")
-                [[ -z "$1" ]] && _echo_error "Missing argument for --new"
-                arg_new="$1"; shift;
+                if [[ -z "$1" || "$1" == -* ]]; then
+                    arg_new="$(basename "$(realpath .)")"
+                else
+                    arg_new="$1"; shift;
+                fi
                 continue;;
         esac
 
-        [[ "$arg" == "-"* ]] && _echo_error "Illegal option: $arg"
+        [[ "$arg" == "-"* ]] && _echo_error "Unknown option: $arg"
         positional+=("$arg")
     done
     unfunction _echo_error
 
     if [[ -n "$arg_help" ]]; then
         cat <<EOF
-usage: pyv [-h] [-l] [-n "new_env"] ["activate_env"]
+usage: pyv [-h] [-l] [-n ["new_env"]]
 
 options:
-  -h, --help            show this help message and exit
-  -l, --list            list environments
-  -n, --new "new_env"   create a new environment named "new_env" and activate it
-  -p, --no-ipython      don't install ipykernel for a new environment
-  "activate_env"        activate given environment
+  -h, --help              show this help message and exit
+  -l, --location          show path of pyv for current directory if exists
+  -n, --new ["new_env"]   create a new environment named "new_env" (default
+                          name of current directory) and activate it
+  -p, --no-ipython        don't install ipykernel for a new environment
 EOF
         return
     fi
     # MARK: ARUGMENT PARSING done ----------------------------------------------
 
     if [[ -n "$arg_new" ]]; then
-        local new_path="$pyv_path/$arg_new"
-        if test -e "$new_path"; then
-            echo "Environment \"$arg_new\" already exists, exiting." >&2
+        local new_path=".pyv"
+        if test -d "$new_path"; then
+            echo "pyv already exists here, exiting." >&2
             return
         fi
 
-        /usr/bin/env python3 -m venv "$new_path"
-        pyv "$arg_new"
+        /usr/bin/env python3 -m venv "$new_path" --prompt "pyv:$arg_new"
+        source "$new_path/bin/activate"
 
         if [[ -z "$arg_noipython" ]]; then
             pip install ipykernel
@@ -59,29 +59,38 @@ EOF
             # iphython kernel install fucks up the environment so we have to
             # reactivate it
             deactivate
-            pyv "$arg_new"
+            source "$new_path/bin/activate"
             pip install jupyterlab
         fi
         return
     fi
 
-    if [[ -n "$arg_list" ]]; then
-        ls "$pyv_path"
+    if [[ -n "$arg_location" ]]; then
+        local pyv_dir=$(_pyv_find)
+        [[ -n "$pyv_dir" ]] && echo "$pyv_dir"
         return
     fi
+}
 
-    local activate_env="$positional[1]"
-    if [[ -n "$activate_env" ]]; then
-        local act_path="$pyv_path/$activate_env/bin/activate"
-        if ! test -f "$act_path"; then
-            echo "Environment \"$activate_env\" does not exist or is broken." >&2
-            return
+function _pyv_find() {
+    while [[ "$(pwd)" != "/" ]]; do
+        if test -d .pyv; then
+            echo "$(realpath .)/.pyv"
+            break
         fi
-        source "$act_path"
-    fi
+        cd ..
+    done
 }
 
-function _pyv() {
-    compadd "${(@)${(f)$(pyv -l)}}"
+function _pyv_cd() {
+    # deactivate if we can
+    which deactivate >/dev/null && deactivate
+
+    local pyv_dir=$(_pyv_find)
+    # if there is no (valid) pyv_dir return
+    {[[ -z "$pyv_dir" ]] || ! test -f "$pyv_dir/bin/activate"} && return
+    # else we can activate
+    source "$pyv_dir/bin/activate"
 }
-compdef _pyv pyv
+autoload -U add-zsh-hook
+add-zsh-hook chpwd _pyv_cd
