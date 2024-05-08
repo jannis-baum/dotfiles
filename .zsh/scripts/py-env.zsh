18e6827d4046de9e21d5fe1d9666378629f17bbd
Jannis Baum <jannis.baum@gmail.com>
2024-05-08 10:46:07 +0200

    zsh: pyv new command structure

--- .zsh/scripts/py-env.zsh
+++ .zsh/scripts/py-env.zsh
@@ -1,88 +1,25 @@
 function pyv() {
-    # MARK: ARUGMENT PARSING ---------------------------------------------------
-    # from https://gist.github.com/jannis-baum/d3e5744466057f4e61614744a2397fdd
-    local arg_help="" positional=() arg_location arg_new_version arg_new_name arg_noipython arg_noreqs
+    case "$1" in
+        "new")
+            shift
+            _pyv_new $@
+            ;;
+        "location")
+            local pyv_dir=$(_pyv_find)
+            [[ -n "$pyv_dir" ]] && echo "$pyv_dir"
+            ;;
+        *) cat <<EOF
+usage: pyv [new | location | help]
 
-    while (( $# )); do
-        _echo_error() {
-            echo "$1" >&2
-            arg_help=1; break
-        }
-        local arg="$1"; shift
+Minimal zsh-centered Python virtual env management using pyenv, venv & pip
 
-        case "$arg" in
-            "-h" | "--help") arg_help=1; continue;;
-            "-l" | "--location") arg_location=1; continue;;
-            "-np" | "--no-ipython") arg_noipython=1; continue;;
-            "-nr" | "--no-requirements") arg_noreqs=1; continue;;
-            "-n" | "--new")
-                [[ -z "$1" || "$1" == -* ]] && _echo_error "Version not specified"
-                arg_new_version="$1"
-                [[ -z "$2" || "$2" == -* ]] \
-                    && arg_new_name="$(basename "$(realpath .)")" \
-                    || arg_new_name="$2"; shift;
-                continue;;
-        esac
-
-        [[ "$arg" == "-"* ]] && _echo_error "Unknown option: $arg"
-        positional+=("$arg")
-    done
-    unfunction _echo_error
-
-    if [[ -n "$arg_help" ]]; then
-        cat <<EOF
-usage: pyv [-h] [-l] [-n ["new_env"]]
-
-options:
-  -h, --help                    show this help message and exit
-  -l, --location                show path of pyv for current directory if
-                                one exists
-  -n, --new version [new_env]   - create a new environment with "version" named
-                                  "new_env" (default name of current directory)
-                                - activate it
-                                - install requirements.txt if exist
-                                - install ipython kernel unless -p is specified
-  -np, --no-ipython             don't install ipykernel for a new environment
-  -nr, --no-requirements        don't install requirements.txt
+positional aguments
+  help       show this help message and exit
+  new        create a new environment, see \`pyv new --help\`
+  location   show path of pyv for current directory if one exists
 EOF
-        return
-    fi
-    # MARK: ARUGMENT PARSING done ----------------------------------------------
-
-    if [[ -n "$arg_new_name" ]]; then
-        local new_path=".pyv"
-        if test -d "$new_path"; then
-            echo "pyv already exists here, exiting." >&2
-            return
-        fi
-
-        PYENV_VERSION="$arg_new_version" pyenv exec python \
-            -m venv "$new_path" --prompt "pyv:$arg_new_name" \
-            || return 1
-        source "$new_path/bin/activate"
-
-        if [[ -z "$arg_noipython" ]]; then
-            pip install ipykernel
-            ipython kernel install --user --name="$arg_new_name"
-            # iphython kernel install fucks up the environment so we have to
-            # reactivate it
-            deactivate
-            source "$new_path/bin/activate"
-            pip install jupyterlab
-        fi
-
-        if [[ -z "$arg_noreqs" ]] && test -f requirements.txt; then
-            pip install -r requirements.txt
-        fi
-
-        return
-    fi
-
-    if [[ -n "$arg_location" ]]; then
-        local pyv_dir=$(_pyv_find)
-        [[ -n "$pyv_dir" ]] && echo "$pyv_dir"
-        return
-    fi
+            ;;
+    esac
 }
 
 function _pyv_new() {
