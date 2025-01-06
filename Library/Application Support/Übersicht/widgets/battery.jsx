b2c63dcaf546939f54d30b953b678df2ee0db31f
Jannis Baum <jannis.baum@gmail.com>
2025-01-06 18:43:47 -0500

    ubersicht: add time & battery widgets

---
+++ Library/Application Support/Übersicht/widgets/battery.jsx
@@ -0,0 +1,9 @@
+export const command = "~/.synapse/status/battery.zsh";
+export const refreshFrequency = 5000;
+export const className =`
+  top: 6px;
+  right: 72px;
+  color: #ddd;
+  font-family: "Poiret One", -apple-system;
+  font-size: 20px;
+`
---
+++ Library/Application Support/Übersicht/widgets/time.jsx
@@ -0,0 +1,9 @@
+export const command = "date +%l:%M";
+export const refreshFrequency = 1000;
+export const className =`
+  top: 6px;
+  right: 12px;
+  color: #ddd;
+  font-family: "Poiret One", -apple-system;
+  font-size: 20px;
+`
