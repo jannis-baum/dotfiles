8ce572561e880c132ead8bc3c1503d17f81c6656
Jannis Baum <jannis.baum@gmail.com>
2025-03-05 08:45:27 +0100

    feat(#8): install paq

--- .config/nvim/plugin/bootstrap.lua
+++ .config/nvim/plugin/bootstrap.lua
@@ -1,3 +1,31 @@
+-- PACK & PLUGINS -----------------------------------------------------
+local function clone_paq()
+    local path = vim.fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
+    local is_installed = vim.fn.empty(vim.fn.glob(path)) == 0
+    if not is_installed then
+        vim.notify('Cloning paq')
+        vim.fn.system { 'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', path }
+        return true
+    end
+end
+
+local function bootstrap_paq(packages)
+    local first_install = clone_paq()
+    vim.cmd.packadd('paq-nvim')
+    local paq = require('paq')
+    if first_install then
+        vim.notify('Installing plugins... If prompted, hit Enter to continue.')
+    end
+
+    paq(packages)
+    paq.install()
+end
+
+bootstrap_paq {
+    'savq/paq-nvim',
+}
+
+-- PYTHON -------------------------------------------------------------
 local python_env_name = 'py3nvim'
 vim.g.python3_host_prog = vim.fn.stdpath('data') .. '/' .. python_env_name .. '/bin/python'
 
