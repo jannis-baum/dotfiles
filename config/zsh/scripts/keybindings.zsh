4f7190e2d27782d50ead14721c71e0bcf0e39d29
Jannis Baum <jannis.baum@gmail.com>
2025-03-08 19:41:20 +0100

    zsh: shared history

--- config/zsh/scripts/keybindings.zsh
+++ config/zsh/scripts/keybindings.zsh
@@ -1,12 +1,21 @@
 # recent commands matching prefix
 # up
-autoload -U up-line-or-beginning-search
-zle -N up-line-or-beginning-search
-bindkey -M vicmd k up-line-or-beginning-search
+autoload -U up-line-or-history
+# helper to re-read history file to get shared commands from other sessions
+up-line-or-history-reread() {
+        [[ -z $BUFFER ]] && fc -R $HISTFILE
+        zle up-line-or-history
+}
+zle -N up-line-or-history-reread
+bindkey  -M vicmd k up-line-or-history-reread
 # down
-autoload -U down-line-or-beginning-search
-zle -N down-line-or-beginning-search
-bindkey -M vicmd j down-line-or-beginning-search
+autoload -U down-line-or-history
+down-line-or-history-reread() {
+        [[ -z $BUFFER ]] && fc -R $HISTFILE
+        zle down-line-or-history
+}
+zle -N down-line-or-history-reread
+bindkey  -M vicmd j down-line-or-history-reread
 
 # shift+tab to go back in completion
 bindkey -M viins "${terminfo[kcbt]}" reverse-menu-complete
--- config/zsh/scripts/misc.zsh
+++ config/zsh/scripts/misc.zsh
@@ -18,7 +18,11 @@ function mue() {
 # options
 setopt no_beep
 ## history
-setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt hist_ignore_dups       # ignore duplicated commands history list
-setopt hist_ignore_space      # ignore commands that start with space
-setopt hist_verify            # show command with history expansion to user before running it
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size
+                                  # exceeds HISTSIZE.
+setopt hist_ignore_dups       # ignore duplicated commands history list.
+setopt hist_ignore_space      # ignore commands that start with space.
+setopt hist_verify            # show command with history expansion to user
+                                  # before running it.
+setopt share_history          # live-append to history file, share items
+                                  # between sessions
