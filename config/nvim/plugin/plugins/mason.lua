c1842d94646df06218abe297e00f85776a4dbc7c
Jannis Baum <jannis.baum@gmail.com>
2025-04-03 23:35:11 +0200

    vim: add link for lsp server list

--- config/nvim/plugin/plugins/mason.lua
+++ config/nvim/plugin/plugins/mason.lua
@@ -1,10 +1,13 @@
 require('mason').setup()
 
 require('mason-lspconfig').setup({
+    -- available LSP servers:
+    -- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
     ensure_installed = {
         'lua_ls',
     }
 })
+
 -- automatic dynamic server setup
 require('mason-lspconfig').setup_handlers({
     function (server_name) -- default handler
