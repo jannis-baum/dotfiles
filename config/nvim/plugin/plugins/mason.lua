eba681ff897faed59d0059f7a6b698cbb631496d
Jannis Baum <jannis.baum@gmail.com>
2025-04-04 15:17:37 +0200

    vim: add language support

--- config/nvim/plugin/plugins/mason.lua
+++ config/nvim/plugin/plugins/mason.lua
@@ -4,8 +4,13 @@ require('mason-lspconfig').setup({
     -- available LSP servers:
     -- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
     ensure_installed = {
-        'lua_ls',
-        'vimls',
+        'lua_ls', 'vimls',
+        'pyright',
+        'bashls',
+        'clangd', 'cmake',
+        'dockerls', 'docker_compose_language_service',
+        'ts_ls', 'eslint', 'jsonls',
+        'html', 'cssls', 'tailwindcss',
     }
 })
 
