4af5813cc55db2306b5560bec14e944e16b2b860
Jannis Baum <jannis.baum@gmail.com>
2025-04-04 17:43:44 +0200

    vim: mini notifications style

--- config/nvim/plugin/plugins/mini.lua
+++ config/nvim/plugin/plugins/mini.lua
@@ -1,3 +1,11 @@
-require('mini.notify').setup()
-vim.notify = MiniNotify.make_notify()
-vim.keymap.set('n', '<leader>m', MiniNotify.show_history, { noremap = true, silent = true })
+local notify = require('mini.notify')
+
+notify.setup({
+    window = {
+        config = { border = 'solid' },
+        winblend = 0,
+    },
+})
+
+vim.notify = notify.make_notify({})
+vim.keymap.set('n', '<leader>m', notify.show_history, { noremap = true, silent = true })
