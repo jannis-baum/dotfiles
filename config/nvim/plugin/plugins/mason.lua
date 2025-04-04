643a4459c467fce300e1da4b0d4c85adf51b98c7
Jannis Baum <jannis.baum@gmail.com>
2025-04-04 14:47:59 +0200

    vim: add vimscript lsp

--- config/nvim/plugin/plugins/mason.lua
+++ config/nvim/plugin/plugins/mason.lua
@@ -5,6 +5,7 @@ require('mason-lspconfig').setup({
     -- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
     ensure_installed = {
         'lua_ls',
+        'vimls',
     }
 })
 
