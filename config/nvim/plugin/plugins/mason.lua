4390cae7a221f3940366316bc9ef1631f859399d
Jannis Baum <jannis.baum@gmail.com>
2025-04-05 11:12:45 +0200

    vim: lua_ls nvim api

--- config/nvim/plugin/plugins/mason.lua
+++ config/nvim/plugin/plugins/mason.lua
@@ -12,12 +12,37 @@ require('mason-lspconfig').setup({
         'yamlls', 'jsonls',
         'ts_ls', 'eslint',
         'html', 'cssls', 'tailwindcss',
-    }
+    },
+    automatic_installation = true,
 })
 
 -- automatic dynamic server setup
 require('mason-lspconfig').setup_handlers({
     function (server_name) -- default handler
         require('lspconfig')[server_name].setup {}
+    end,
+    ['lua_ls'] = function()
+        require('lspconfig').lua_ls.setup {
+            settings = {
+                Lua = {
+                    runtime = {
+                        -- LuaJIT for NeoVim
+                        version = "LuaJIT",
+                    },
+                    diagnostics = {
+                        -- global `vim`
+                        globals = { "vim" },
+                    },
+                    workspace = {
+                        -- Neovim runtime files
+                        library = vim.api.nvim_get_runtime_file("", true),
+                    },
+                    -- avoid telemetry data
+                    telemetry = {
+                        enable = false,
+                    },
+                }
+            }
+        }
     end
 })
