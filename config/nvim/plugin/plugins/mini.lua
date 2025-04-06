5a645fd2abc22b57d9de8bb68ae61068f1deacc5
Jannis Baum <jannis.baum@gmail.com>
2025-04-06 12:31:38 +0200

    vim: clean up mini surround maps

--- config/nvim/plugin/plugins/mini.lua
+++ config/nvim/plugin/plugins/mini.lua
@@ -21,19 +21,22 @@ require('mini.ai').setup({
     }
 })
 
+-- remove default mapping of ; that we don't need because of sneak and instead
+-- use for surround
+vim.keymap.del('n', ';')
 require('mini.surround').setup({
     search_method = 'cover_or_next',
     mappings = {
-        add = ';a',        -- Add surrounding in Normal and Visual modes
-        delete = ';d',     -- Delete surrounding
-        find = ';f',       -- Find surrounding (to the right)
-        find_left = ';F',  -- Find surrounding (to the left)
-        highlight = ';h',  -- Highlight surrounding
-        replace = ';r',    -- Replace surrounding
-        update_n_lines = ';n', -- Update `n_lines`
+        add = ';a',          -- add surrounding in Normal and Visual modes
+        delete = ';d',       -- delete surrounding
+        replace = ';r',      -- replace surrounding
+        find = '',           -- find surrounding (to the right)
+        find_left = '',      -- find surrounding (to the left)
+        highlight = '',      -- highlight surrounding
+        update_n_lines = '', -- update `n_lines`
 
-        suffix_last = 'l', -- Suffix to search with "prev" method
-        suffix_next = 'n', -- Suffix to search with "next" method
+        suffix_last = 'l', -- suffix to search with "prev" method
+        suffix_next = 'n', -- suffix to search with "next" method
     },
     custom_surroundings = {
         -- bracket aliases
