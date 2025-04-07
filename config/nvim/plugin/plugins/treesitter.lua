58cbb1f8c88254ffc4701e60a91736b6e224e13d
Jannis Baum <jannis.baum@gmail.com>
2025-04-07 13:26:30 +0200

    vim: fzf treesitter symbol picker

--- config/nvim/plugin/plugins/treesitter.lua
+++ config/nvim/plugin/plugins/treesitter.lua
@@ -18,7 +18,7 @@ require 'nvim-treesitter.configs'.setup({
     modules = {}
 })
 
-local main_symbols = {
+local relevant_symbols = {
     'function_declaration', 'function_definition',
     'class_declaration', 'class_definition'
 }
@@ -28,12 +28,29 @@ local blacklist_symbols = {
 
 local function inspect_node(node, depth, storage)
     local type = node:type()
-    if (depth == 1 and not vim.list_contains(blacklist_symbols, type)) or vim.list_contains(main_symbols, type) then
+    if (depth == 1 and not vim.list_contains(blacklist_symbols, type)) or vim.list_contains(relevant_symbols, type) then
         table.insert(storage, { depth = depth, node = node })
     end
     for _, child in ipairs(node:named_children()) do
         inspect_node(child, depth + 1, storage)
     end
+end
+
+local function symbol_nodes()
+    local parser = vim.treesitter.get_parser(0)
+    if parser == nil then return end
+    local parse_result = parser:parse(true)
+    if parse_result == nil then return end
+
+    local root = parse_result[1]:root()
+    local results = {}
+    inspect_node(root, 0, results)
+    return results
+end
+
+local function fzf_sink(line)
+    local row, column = string.match(line, '([^:]+):([^:]+)')
+    vim.fn.setpos('.', { 0, row, column })
 end
 
 local function fzf_line(node, depth)
@@ -44,18 +61,26 @@ local function fzf_line(node, depth)
     return tostring(row) .. ':' .. tostring(column) .. ':' .. text
 end
 
-function MainSymbols()
-    local parser = vim.treesitter.get_parser(0)
-    if parser == nil then return end
-    local parse_result = parser:parse(true)
-    if parse_result == nil then return end
+function SelectSymbol()
+    local symbols = symbol_nodes()
+    if symbols == nil then return end
 
-    local root = parse_result[1]:root()
-    local results = {}
-    inspect_node(root, 0, results)
-    local fzf_lines = {}
-    for _, result in pairs(results) do
-        table.insert(fzf_lines, fzf_line(result['node'], result['depth']))
+    local fzf_input = {}
+    for _, symbol in pairs(symbols) do
+        table.insert(fzf_input, fzf_line(symbol['node'], symbol['depth']))
     end
-    return table.concat(fzf_lines, '\n')
+
+    local fzf_wrap = vim.fn['fzf#wrap']({
+        source = fzf_input,
+        options = {
+            '--delimiter', ':',
+            '--with-nth', '3..'
+        },
+        sink = fzf_sink
+    })
+    vim.fn['fzf#run'](fzf_wrap)
 end
+
+vim.keymap.set('n', '<cr>', function()
+    SelectSymbol()
+end, { noremap = true })
