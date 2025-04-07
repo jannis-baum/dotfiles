4f782e118c85dfb4b660d76d619439d3855ca039
Jannis Baum <jannis.baum@gmail.com>
2025-04-07 08:15:45 +0200

    vim: treesitter update config

--- config/nvim/plugin/plugins/treesitter.lua
+++ config/nvim/plugin/plugins/treesitter.lua
@@ -1,18 +1,19 @@
-require'nvim-treesitter.configs'.setup({
-  ensure_installed = 'all',
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
-  -- List of parsers to ignore installing (or "all")
-  ignore_install = {
-      'comment', -- too complicated comment parsing
-      'luadoc',  -- very broken and inconsistent, looks best as just comment
-      'luap'     -- too complicated regex parsing in lua
-  },
+require 'nvim-treesitter.configs'.setup({
+    ensure_installed = 'all',
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
+    -- List of parsers to ignore installing (or "all")
+    ignore_install = {
+        'comment', -- too complicated comment parsing
+        'luadoc', -- very broken and inconsistent, looks best as just comment
+        'luap'   -- too complicated regex parsing in lua
+    },
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  }
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    modules = {}
 })
