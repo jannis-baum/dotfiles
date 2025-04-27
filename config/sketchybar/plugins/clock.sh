0e0ca8910fc78a0e78326c6d10ce4047bf4022e5
Jannis Baum <jannis.baum@gmail.com>
2025-04-27 14:23:42 +0200

    sketchybar: hoverable time

--- config/sketchybar/plugins/clock.sh
+++ config/sketchybar/plugins/clock.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-# | xargs to trim whitespace
-sketchybar --set "$NAME" label="$(date +%l:%M | xargs)"
+NAME_ORIG="$(echo "$NAME" | sed 's/-hover$//')"
+NAME_NEW="$NAME"
+
+case "$SENDER" in
+  "mouse.entered") NAME_NEW="$NAME_ORIG-hover";;
+  "mouse.exited") NAME_NEW="$NAME_ORIG";;
+esac
+[[ "$NAME" != "$NAME_NEW" ]] && sketchybar --rename "$NAME" "$NAME_NEW"
+
+# | xargs trims whitespace
+[[ "$NAME_NEW" = *-hover ]] \
+    && label=$(date +%m/%d | xargs | tr -d '\n'; printf "  "; date +%l:%M:%S | xargs) \
+    || label=$(date +%l:%M | xargs)
+
+sketchybar --set "$NAME_NEW" label="$label"
