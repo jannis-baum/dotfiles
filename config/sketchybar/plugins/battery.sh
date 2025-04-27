0a736298074482e8e184ae5827711065778aa982
Jannis Baum <jannis.baum@gmail.com>
2025-04-27 14:38:29 +0200

    sketchybar: battery widget

---
+++ config/sketchybar/plugins/battery.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+source "$CONFIG_DIR/helpers/hover.sh"
+
+BATTERY=$(pmset -g batt)
+
+if [[ -z "$BATTERY" || "$BATTERY" == *charged* || "$BATTERY" == *"finishing charge"* ]]; then
+    sketchybar --set "$NAME" drawing=off
+    exit 0
+fi
+
+sketchybar --set "$NAME" drawing=on
+
+CHARGING="$([[ "$BATTERY" == *discharging* ]] || printf '⚡ ')"
+
+[[ "$NAME" = *-hover ]] \
+    && LABEL="$CHARGING$(grep -o '\d\+%' <<< $BATTERY)" \
+    || LABEL="$CHARGING$(grep -o '\d\+:\d\+' <<< $BATTERY || echo '···')"
+
+sketchybar --set "$NAME" label="$LABEL"
