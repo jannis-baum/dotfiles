b2cd0ab91dea751ce787be6ddbd64cc572cac426
Jannis Baum <jannis.baum@gmail.com>
2025-04-28 09:05:40 +0200

    kitty: use `home` instead of `~` for tab titles

--- config/kitty/watcher.py
+++ config/kitty/watcher.py
@@ -104,7 +104,7 @@ def _refresh_widgets(boss: Boss) -> None:
         active_id = (active_tab := tab_manager.active_tab) and active_tab.id
         def get_title(tab) -> str:
             cwd = tab.get_cwd_of_active_window() or '??'
-            cwd = cwd.replace(os.path.expanduser('~'), '~')
+            cwd = cwd.replace(os.path.expanduser('~'), 'home')
             cwd = cwd.split('/')[-1]
             prefix = git_info if tab.id == active_id else 'FAINT '
             return prefix + cwd
