59df2bf5f07c66ead19009e735dc157eb034d6d6
Jannis Baum <jannis.baum@gmail.com>
2025-05-11 10:01:09 +0200

    vim: adjust jupyviv bindings

--- config/nvim/plugin/plugins/jupyviv.lua
+++ config/nvim/plugin/plugins/jupyviv.lua
@@ -1,3 +1,5 @@
 require('jupyviv').setup()
 
-vim.keymap.set('n', '<leader>r', Jupyviv.run_here)
+vim.keymap.set('n', '<leader>jr', Jupyviv.run_here)
+vim.keymap.set('n', '<leader>ja', Jupyviv.run_all)
+vim.keymap.set('n', '<leader>j;', ':Jupyviv')
