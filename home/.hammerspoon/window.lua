5790bfe0613dd16988dbb7eae5feccdbff490ecb
Jannis Baum <jannis.baum@gmail.com>
2025-05-14 21:36:38 +0200

    hammerspoon: different hack for auto window resizing

--- home/.hammerspoon/window.lua
+++ home/.hammerspoon/window.lua
@@ -1,5 +1,24 @@
-hs.window.animationDuration = 0 -- does nothing
+-- AXEnhancedUserInterface setting breaks window moving & resizing so we
+-- disable and then reset it to do these actions on windows
+-- (https://github.com/Hammerspoon/hammerspoon/issues/3224#issuecomment-1294359070)
+local function ax_enhanced_workaround(app, action)
+    local ax_app = hs.axuielement.applicationElement(app)
 
+    -- original settings
+    local was_enhanced = ax_app.AXEnhancedUserInterface
+    local original_animation_duration = hs.window.animationDuration
+
+    -- set & run action
+    ax_app.AXEnhancedUserInterface = false
+    hs.window.animationDuration = 0
+    action()
+
+    -- restore original settings
+    hs.window.animationDuration = original_animation_duration
+    ax_app.AXEnhancedUserInterface = was_enhanced
+end
+
+-- fit window below sketchybar on external displays
 local sketchybar_height = 32
 local function fit_win_below_sketchybar(window)
     local screen = window:screen()
@@ -13,11 +32,12 @@ local function fit_win_below_sketchybar(window)
     if overlap <= 0 then
         return
     end
-    -- horrible hack to move & resize window
-    -- https://github.com/Hammerspoon/hammerspoon/issues/3731
-    window:setSize({ w = win_frame.w, h = win_frame.h - overlap })
-    os.execute('sleep 0.25')
-    window:setTopLeft({ x = win_frame.x, y = win_frame.y + overlap })
+    ax_enhanced_workaround(window:application(), function()
+        window:setFrame({
+            x = win_frame.x, y = win_frame.y + overlap,
+            w = win_frame.w, h = win_frame.h - overlap
+        }, 0)
+    end)
 end
 
 hs.urlevent.bind('fitWinBelowSketchybar', function(eventName, params)
