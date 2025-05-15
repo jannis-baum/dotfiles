424d0569a5743d61c9c9001afccebd11539f23b3
Jannis Baum <jannis.baum@gmail.com>
2025-05-15 15:11:06 +0200

    zsh: use uv for auto-venvs

--- config/zsh/scripts/uvz.zsh
+++ config/zsh/scripts/uvz.zsh
@@ -15,7 +15,7 @@ Minimal zsh-centered Python virtual env management using uv
 
 positional aguments
   help       show this help message and exit
-  new        create a new environment, see \`uvz new --help\`
+  new        create a new project/environment, see \`uvz new --help\`
   location   show path of uvz for current directory if one exists
 EOF
             ;;
@@ -25,7 +25,7 @@ EOF
 function _uvz_new() {
     # MARK: ARUGMENT PARSING ---------------------------------------------------
     # from https://gist.github.com/jannis-baum/d3e5744466057f4e61614744a2397fdd
-    local arg_help="" positional=() arg_version arg_jupyter arg_noreqs
+    local arg_help="" positional=() set_uv_args uv_args=()
 
     while (( $# )); do
         _echo_error() {
@@ -34,14 +34,14 @@ function _uvz_new() {
         }
         local arg="$1"; shift
 
+        if [[ -n "$set_uv_args" ]]; then
+            uv_args+=("$arg")
+            continue
+        fi
+
         case "$arg" in
             "-h" | "--help") arg_help=1; continue;;
-            "-j" | "--jupyter") arg_jupyter=1; continue;;
-            "-nr" | "--no-requirements") arg_noreqs=1; continue;;
-            "-v" | "--version")
-                [[ -z "$1" || "$1" == -* ]] && _echo_error "Version not specified"
-                arg_version="$1"; shift;
-                continue;;
+            "-") set_uv_args=1; continue;;
         esac
 
         [[ "$arg" == "-"* ]] && _echo_error "Unknown option: $arg"
@@ -51,63 +51,35 @@ function _uvz_new() {
 
     if [[ -n "$arg_help" ]]; then
         cat <<EOF
-usage: uvz new [-h] [-np] [-nr] [-v] [name]
+usage: uvz new [-h] [- [uv init args ...]]
 
-Create a new environment and activate it
+Create a new project and environment and activate it, or create only the
+environment if there is already a project.
 
 positional arugments:
-  name                          name for the new environment (defaults to
-                                basename of current directory)
+  name                   Name for the new environment (defaults to basename of
+                         current directory).
 
 options:
-  -h, --help                    show this help message and exit
-  -v, --version                 the python version to use, defaults to locally
-                                set pyenv version if exists
-  -j, --jupyter                 install Jupyter Lab for the new environment
-  -nr, --no-requirements        don't install existing requirements.txt
+  -h, --help             Show this help message and exit.
+  -                      Use all additional arguments for \`uv init\`.
 EOF
         return
     fi
     # MARK: ARUGMENT PARSING done ----------------------------------------------
 
-    local arg_name="$positional[1]"
-    [[ -z "$arg_name" ]] \
-        && arg_name="$(basename "$(realpath .)")"
-
     local new_path=".venv"
     if test -d "$new_path"; then
-        echo "uvz already exists here, exiting." >&2
+        echo "uvz environment already exists here, exiting." >&2
         return
     fi
 
-    _error_version() {
-        echo "Available versions are:"
-        pyenv versions | grep -v "system"
-    }
-
-    if [[ -n "$arg_version" ]]; then
-        if ! pyenv local "$arg_version" 2>/dev/null; then
-            echo "Version $arg_version is not available."
-            _error_version
-            return 1
-        fi
+    if ! test -f "pyproject.toml"; then
+        uv init "${uv_args[@]}" || return
     fi
+    uv venv
 
-    if ! pyenv exec python -m venv "$new_path" --prompt "uvz:$arg_name" 2>/dev/null; then
-        echo 'Please specify a version with `-v` or set it locally with `pyenv local "version"`.' >&2
-        _error_version
-        return 1
-    fi
-    unfunction _error_version
     source "$new_path/bin/activate"
-
-    if [[ -n "$arg_jupyter" ]]; then
-        pip install jupyterlab
-    fi
-
-    if [[ -z "$arg_noreqs" ]] && test -f requirements.txt; then
-        pip install -r requirements.txt
-    fi
 }
 
 function _uvz_find() {
