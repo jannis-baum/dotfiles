98b20d702f2234a86bd796855a256084913f9088
Jannis Baum <jannis.baum@gmail.com>
2025-05-17 16:30:53 +0200

    zsh: adjust trm alias

--- config/zsh/scripts/misc.zsh
+++ config/zsh/scripts/misc.zsh
@@ -3,7 +3,7 @@ alias c="printf '\n%.0s' {2..$LINES} && clear"
 alias ej="diskutil eject /Volumes/LaCie*"
 alias w='printf "%s" "$(VISUAL=nvim vipe)" | pbcopy'
 alias ssh='kitty +kitten ssh'
-alias trm='trash -F'
+alias trm='trash'
 alias g='gsi'
 alias stitch-jpegs="cat *.jpeg | ffmpeg -f image2pipe -vcodec mjpeg -i - -vcodec libx264 out.mp4"
 alias screenshot='screencapture -i'
