1e4a3c4baa1a5f1df64a2f3b3cc6ee638a8d03c3
Jannis Baum <jannis.baum@gmail.com>
2025-05-26 07:36:56 +0200

    vim: remove autonohl

--- config/nvim/plugin/events.lua
+++
@@ -1,8 +0,0 @@
--- auto nohlsearch from @tuurep/dotfiles
-local searchkeys = { "n", "N", "*", "#" }
-vim.on_key(function(key)
-    if vim.v.hlsearch == 0 then return end
-    if not vim.tbl_contains(searchkeys, key) then
-        vim.cmd.nohlsearch()
-    end
-end)
