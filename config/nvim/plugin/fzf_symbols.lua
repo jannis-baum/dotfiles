49788085f6ab62ba2d1a0b5fa835700c153ece75
Jannis Baum <jannis.baum@gmail.com>
2025-06-02 10:17:01 +0200

    vim: add markdown headings to symbol picker

--- config/nvim/plugin/fzf_symbols.lua
+++ config/nvim/plugin/fzf_symbols.lua
@@ -14,6 +14,7 @@ local relevant_symbols = {
     'field_declaration',    -- C++
     'friend_declaration',   -- C++
     'lexical_declaration',  -- TypeScript
+    'atx_heading',          -- Markdown
 }
 local blacklist_symbols = {
     'comment',               -- everything
