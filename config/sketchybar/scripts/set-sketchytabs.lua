142262d0b7af1dfbbd83129cf2c538ca9f9944a4
Jannis Baum <jannis.baum@gmail.com>
2025-06-16 10:32:35 +0200

    sketchybar: lua set sketchytabs with explicit images

--- config/sketchybar/scripts/set-sketchytabs.lua
+++ config/sketchybar/scripts/set-sketchytabs.lua
@@ -5,12 +5,17 @@ local function get_current_app()
     return app
 end
 
+local function assert_non_nil(value)
+    if value == nil then
+        print('usage: ' .. arg[0] .. ' APP')
+        print('Tabs are read from stdin as `[active]:[image-path]:tab title`')
+        os.exit(1)
+    end
+end
+
 local function main()
     local tab_app = arg[1]
-    if tab_app == nil then
-        print('usage: ' .. arg[0] .. ' APP')
-        return 1
-    end
+    assert_non_nil(tab_app)
 
     local current_app = get_current_app()
     if current_app == nil then return end
@@ -20,9 +25,13 @@ local function main()
 
     local line_num = 1
     for line in io.read('*a'):gmatch('[^\n]+') do
+        local is_active, image, label = line:match('([^:]*):([^:]*):(.*)')
+        assert_non_nil(is_active)
+        assert_non_nil(image)
+        assert_non_nil(label)
+
         local item_name = 'APP-' .. tab_app .. '-' .. tostring(line_num)
-        local label, is_active = line:gsub('ACTIVE:', '')
-        local label_color = (is_active > 0) and '0xffbbbbbb' or '0xff808080'
+        local label_color = (is_active ~= '') and '0xffbbbbbb' or '0xff808080'
 
         sketchy_cmd = sketchy_cmd
             .. ' --add item "' .. item_name .. '" left'
@@ -33,6 +42,15 @@ local function main()
                 .. ' label.font=Menlo:Bold:14'
                 .. ' label.color=' .. label_color
             .. ' --subscribe "' .. item_name .. '" front_app_switched'
+
+        if image ~= '' then
+            sketchy_cmd = sketchy_cmd .. ' --set "' .. item_name .. '"'
+                .. ' background.drawing=on background.image.drawing=on'
+                .. ' icon.padding_left=8'
+                .. ' background.padding_left=8'
+                .. ' background.image.string="' .. image .. '"'
+                .. ' background.image.scale=0.5'
+        end
 
         line_num = line_num + 1
     end
