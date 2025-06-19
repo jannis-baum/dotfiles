707f1d6a0716f4118f260eb071e1d6afa11865d4
Jannis Baum <jannis.baum@gmail.com>
2025-06-19 16:26:47 +0200

    sketchybar: cpu warning

---
+++ config/sketchybar/plugins/cpu-busy.zsh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+# appear when CPU usage goes above threshold, mostly to detect stuck busy
+# processes
+
+source "$CONFIG_DIR/helpers/hover.sh"
+
+# ps:
+#   -A for all user processes
+#   -r to sort by CPU
+#   -o is format
+#   comm is command
+# head/tail to get max cpu & remove header
+# xargs to trim whitespace
+ps_out="$(ps -Aro %cpu,comm | head -2 | tail -1 | xargs)"
+cpu="$(cut -w -f1 <<<"$ps_out")"
+
+if [[ "$cpu" -lt 90 ]]; then
+    sketchybar --set "$NAME" drawing=off
+    exit 0
+fi
+
+sketchybar --set "$NAME" drawing=on
+
+[[ "$NAME" = *-hover ]] \
+    && LABEL="$(cut -w -f2- <<<"$ps_out")" \
+    || LABEL="$cpu%"
+
+sketchybar --set "$NAME" label="$LABEL"
