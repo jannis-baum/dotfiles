b91562723b116e213f5be0303a3fe0c751020822
Jannis Baum <jannis.baum@gmail.com>
2025-07-02 12:24:25 +0200

    karabiner: adjust sim threshold

--- lib/karabiner-config/src/keymap.ts
+++ lib/karabiner-config/src/keymap.ts
@@ -82,7 +82,7 @@ writeToProfile('karabiner.ts',
        'basic.to_if_alone_timeout_milliseconds': 300,
        'basic.to_if_held_down_threshold_milliseconds': 50,
        'basic.to_delayed_action_delay_milliseconds': 0,
-       'basic.simultaneous_threshold_milliseconds': 30,
+       'basic.simultaneous_threshold_milliseconds': 20,
     }
 );
 
