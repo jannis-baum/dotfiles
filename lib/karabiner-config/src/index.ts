3fb57cfbb2f4524847ca08f7dbcc5d7eea3e0d46
Jannis Baum <jannis.baum@gmail.com>
2025-07-02 07:11:16 +0200

    karabiner: chars & numbers with .ts

--- lib/karabiner-config/src/index.ts
+++ lib/karabiner-config/src/index.ts
@@ -1,4 +1,10 @@
-import { writeToProfile, map, rule, ifApp, toApp, ifVar, mapSimultaneous } from 'karabiner.ts'
+import {
+    writeToProfile,
+    rule, simlayer,
+    map, mapSimultaneous, withMapper,
+    toApp,
+    ifApp, ifVar,
+} from 'karabiner.ts'
 
 const kVnnoremap = () => ifVar('kVnnoremap', true);
 
@@ -43,6 +49,33 @@ writeToProfile('karabiner.ts',
             mapSimultaneous([',', '.']).condition(ifApp('Xcode').unless(), kVnnoremap()).to('a').to('⇥', '⌃').to('⎋'),
             mapSimultaneous([',', '.']).condition(ifApp('Xcode'), kVnnoremap().unless()).to(']', ['⌘', '⇧']),
             mapSimultaneous([',', '.']).condition(ifApp('Xcode'), kVnnoremap()).to('a').to(']', ['⌘', '⇧']).to('⎋'),
+        ]),
+
+        simlayer(['a', ';'], 'char-mode').manipulators([
+            // upper row
+            map('q').to('vk_none'), map('w').to('`', '⇧'), map('e').to('`'), map('r').to('6', '⇧'), map('t').to('4', '⇧'),
+            map('y').to('3', '⇧'), map('u').to('8', '⇧'), map('i').to('['), map('o').to(']'), map('p').to('vk_none'),
+            // home row
+            map('s').to('\'', '⇧'), map('d').to('\''), map('f').to('\\', '⇧'), map('g').to('\\'),
+            map('h').to('5', '⇧'), map('j').to('-'), map('k').to('9', '⇧'), map('l').to('0', '⇧'),
+            // lower row
+            map('z').to('vk_none'), map('x').to('=', '⇧'), map('c').to('='), map('v').to('1', '⇧'), map('b').to('2', '⇧'),
+            map('n').to('7', '⇧'), map('m').to('[', '⇧'), map('.').to(']', '⇧'), map('/').to('vk_none')
+        ]),
+
+        simlayer('z', 'number-mode').manipulators([
+            // keypad mapping
+            withMapper({
+                        'u': 7, 'i': 8, 'o': 9,
+                        'j': 4, 'k': 5, 'l': 4,
+                'n': 0, 'm': 1, ',': 2, '.': 3,
+            } as const)((k, i) => map(k).to(`keypad_${i as 0}`)),
+            // disable other keys
+            withMapper([
+                'q', 'w', 'e', 'r', 't', 'y',                'p',
+                'a', 's', 'd', 'f', 'g', 'h',                ';',
+                     'x', 'c', 'v', 'b',                     '/',
+            ] as const)((k) => map(k).to('vk_none'))
         ])
     ],
     {
