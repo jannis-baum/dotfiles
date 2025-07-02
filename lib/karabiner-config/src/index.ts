74bba3ec12c066de7b78d635b630ae55cb8ce1eb
Jannis Baum <jannis.baum@gmail.com>
2025-07-02 05:20:21 +0200

    karabiner: set parameters for .ts

--- lib/karabiner-config/src/index.ts
+++ lib/karabiner-config/src/index.ts
@@ -1,3 +1,11 @@
 import { writeToProfile } from 'karabiner.ts'
 
-writeToProfile('karabiner.ts', [])
+writeToProfile('karabiner.ts',
+    [],
+    {
+       'basic.to_if_alone_timeout_milliseconds': 300,
+       'basic.to_if_held_down_threshold_milliseconds': 50,
+       'basic.to_delayed_action_delay_milliseconds': 0,
+       'basic.simultaneous_threshold_milliseconds': 30,
+    }
+)
