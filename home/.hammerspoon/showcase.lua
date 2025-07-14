924f9aa1856355738d1c9ffe396076b3271c964e
Jannis Baum <jannis.baum@gmail.com>
2025-07-14 11:51:23 +0200

    hammerspoon: showcase type complex keys

--- home/.hammerspoon/showcase.lua
+++ home/.hammerspoon/showcase.lua
@@ -1,8 +1,36 @@
+local shift_keys = {
+    -- number row
+    ["~"] = "`", ["!"] = "1", ["@"] = "2", ["#"] = "3", ["$"] = "4",
+    ["%"] = "5", ["^"] = "6", ["&"] = "7", ["*"] = "8", ["("] = "9",
+    [")"] = "0", ["_"] = "-", ["+"] = "=",
+    -- upper row
+    ["{"] = "[", ["}"] = "]", ["|"] = "\\",
+    -- home row
+    [":"] = ";", ["\""] = "'",
+    -- lower row
+    ["<"] = ",", [">"] = ".", ["?"] = "/",
+}
+for c in string.gmatch("abcdefghijklmnopqrstuvwxyz", ".") do
+    shift_keys[c:upper()] = c
+end
+
+local named_keys = {
+    [" "] = "space"
+}
+
 function Type(input, delay)
     if #input == 0 then return end
     if delay == nil then delay = 30 end
     local char = input:sub(1, 1)
-    hs.eventtap.keyStroke({}, char, 0)
+    local mods = {}
+    if shift_keys[char] ~= nil then
+        char = shift_keys[char]
+        mods = {"shift"}
+    end
+    if named_keys[char] ~= nil then
+        char = named_keys[char]
+    end
+    hs.eventtap.keyStroke(mods, char, 0)
     hs.timer.doAfter(delay / 1000, function()
         Type(input:sub(2), delay)
     end)
