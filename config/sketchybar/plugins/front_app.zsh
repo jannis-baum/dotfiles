c79d0aa33157cb8008a7b62a98e3ff195ee1365c
Jannis Baum <jannis.baum@gmail.com>
2025-09-06 08:10:09 +0200

    sketchybar: don't show finder

--- config/sketchybar/plugins/front_app.zsh
+++ config/sketchybar/plugins/front_app.zsh
@@ -2,7 +2,7 @@
 
 case "$SENDER" in
     front_app_switched)
-        front_app="$INFO"
+        front_app="$([[ "$INFO" == "Finder" ]] && echo "" || echo "$INFO")"
         if sketchybar --query "APP-$front_app-1" &>/dev/null; then
             sketchybar --set "$NAME" drawing=off
         else
@@ -12,7 +12,7 @@ case "$SENDER" in
     tabs_updated)
         current_app="$(osascript -l JavaScript -e 'ObjC.import("AppKit");$.NSWorkspace.sharedWorkspace.frontmostApplication.localizedName')"
         if [[ "$current_app" = "$TAB_APP" ]]; then
-            draw=$([[ "$N_TABS" = 0 ]] && printf "on" || printf "off")
+            draw=$([[ "$N_TABS" = 0 ]] && echo "on" || echo "off")
             sketchybar --set "$NAME" drawing=$draw
         fi
         ;;
