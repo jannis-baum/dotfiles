c46f77ff07b7023c09db3d33dfce4fb362dcd7bc
Jannis Baum <plant.lull_1o@icloud.com>
2025-11-24 18:09:33 +0100

    setup: upgrade sub repo

--- config/nvim/ftplugin/markdown.lua
+++ config/nvim/ftplugin/markdown.lua
@@ -1,3 +1,7 @@
 vim.keymap.set('i', '<c-u>', function()
-    Masi.insert_reference()
+    return Masi.insert_reference()
 end, { noremap = true, silent = true, buffer = true })
+
+vim.keymap.set({'n', 'x'}, '<leader>wc', function()
+    return Masi.count_words()
+end, { expr = true, silent = true, buffer = true })
