b6dbf7896a8fd4c7cc4a62f8391c387fea985cd5
Jannis Baum <plant.lull_1o@icloud.com>
2026-01-08 19:08:56 +0100

    setup/zsh: add nvm

--- config/zsh/plugins-other/plugins.zsh
+++ config/zsh/plugins-other/plugins.zsh
@@ -1,3 +1,11 @@
+# fzf
 source "$ZDOTDIR/plugins-other/fzf-tab-completion/zsh/fzf-zsh-completion.sh"
 bindkey '^I' fzf_completion
 zstyle ':completion:*' fzf-search-display true
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+# load nvm
+[[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]] && source "/opt/homebrew/opt/nvm/nvm.sh"
+# nvm completion
+[[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]] && source "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
