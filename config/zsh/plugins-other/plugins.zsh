34e078e69d0c35db849b26652c3b0ae795805141
Jannis Baum <plant.lull_1o@icloud.com>
2026-01-09 16:52:16 +0100

    zsh: lazy-load nvm

--- config/zsh/plugins-other/plugins.zsh
+++ config/zsh/plugins-other/plugins.zsh
@@ -3,9 +3,24 @@ source "$ZDOTDIR/plugins-other/fzf-tab-completion/zsh/fzf-zsh-completion.sh"
 bindkey '^I' fzf_completion
 zstyle ':completion:*' fzf-search-display true
 
-# nvm
-export NVM_DIR="$HOME/.nvm"
-# load nvm
-[[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]] && source "/opt/homebrew/opt/nvm/nvm.sh"
-# nvm completion
-[[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]] && source "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+# lazy-load nvm because loading it is super slow
+function _lazy_load_nvm {
+    # remove lazy-loading functions
+    unfunction nvm node _lazy_load_nvm
+
+    export NVM_DIR="$HOME/.nvm"
+    # load nvm
+    [[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]] && source "/opt/homebrew/opt/nvm/nvm.sh"
+    # nvm completion
+    [[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]] && source "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+}
+
+function nvm() {
+    _lazy_load_nvm
+    nvm $@
+}
+
+function node() {
+    _lazy_load_nvm
+    node $@
+}
