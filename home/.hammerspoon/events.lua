ea6482a62b470093d77d147bf4b3e99f4cfc6167
Jannis Baum <plant.lull_1o@icloud.com>
2026-01-10 07:46:30 +0100

    hammerspoon: remove unused events

--- home/.hammerspoon/events.lua
+++ home/.hammerspoon/events.lua
@@ -29,41 +29,6 @@ function e.setupDNWatcher(dn, callback)
     e.dnWatchers[#e.dnWatchers]:start()
 end
 
--- karabiner -------------------------------------------------------------------
-function e.karabinerSet(data)
-    hs.execute(
-        "/Library/Application\\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --set-variables '"
-        .. hs.json.encode(data) .. "'")
-end
-
-function e.setupKarabinerDNs(dnTrue, dnFalse, karabinerVariable)
-    e.setupDNWatcher(dnTrue, function() e.karabinerSet({ [karabinerVariable] = 1 }) end)
-    e.setupDNWatcher(dnFalse, function() e.karabinerSet({ [karabinerVariable] = 0 }) end)
-end
-
-e.setupKarabinerDNs('kindaVimDidEnterNormalMode', 'kindaVimDidExitNormalMode', 'kVNormal')
-e.setupKarabinerDNs('WooshyInputDidAppear', 'WooshyInputDidDisappear', 'inWooshy')
-e.setupKarabinerDNs('ScrollaDidEngage', 'ScrollaDidDisengage', 'inScrolla')
-
--- wooshy ----------------------------------------------------------------------
-hs.urlevent.bind('show-menubar', function()
-    pos = hs.mouse.absolutePosition()
-    event = hs.eventtap.event.newEvent()
-    event:setType(hs.eventtap.event.types.mouseMoved)
-    event:location({ x=pos.x, y=0 })
-    event:post()
-end)
-
-hs.urlevent.bind('hide-menubar', function()
-    pos = hs.mouse.absolutePosition()
-    if pos.y > 24 then return end
-
-    event = hs.eventtap.event.newEvent()
-    event:setType(hs.eventtap.event.types.mouseMoved)
-    event:location({ x=pos.x, y=pos.y + 100 })
-    event:post()
-end)
-
 -- dark/light mode -------------------------------------------------------------
 e.setupDNWatcher('AppleInterfaceThemeChangedNotification', function()
     os.execute(os.getenv('HOME') .. '/.config/kitty/scripts/sync-theme')
