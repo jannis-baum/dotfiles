46690a244224ac032c04ec1760f63b5aa473b7c3
Jannis Baum <plant.lull_1o@icloud.com>
2026-01-11 13:44:09 +0100

    sketchybar: update menu bar item index

--- config/sketchybar/scripts/show-control-center.zsh
+++ config/sketchybar/scripts/show-control-center.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 osascript -e \
-    'tell application "System Events" to tell process "Control Center" to click menu bar item 6 of menu bar 1'
+    'tell application "System Events" to tell process "Control Center" to click menu bar item 2 of menu bar 1'
