73bcf7710968ad8850cd7323b8d1f814816d64e4
Jannis Baum <plant.lull_1o@icloud.com>
2026-02-13 13:29:50 -0500

    sketchybar: wip improve cpu busy indicator

--- config/sketchybar/plugins/cpu-busy.zsh
+++ config/sketchybar/plugins/cpu-busy.zsh
@@ -1,24 +1,52 @@
 #!/bin/zsh
 
-# appear when CPU usage goes above threshold, mostly to detect stuck busy
-# processes
+function get_busy_processes() {
+    n_samples=5
+    cpu_thresh=90
 
-# ps:
-#   -A for all user processes
-#   -r to sort by CPU
-#   -o is format
-#   comm is command
-# head/tail to get max cpu & remove header
-# xargs to trim whitespace
-ps_out="$(ps -Aro %cpu,comm | head -2 | tail -1 | xargs)"
-cpu="$(cut -w -f1 <<<"$ps_out")"
+    # get all processes with cpu usage > cpu_thresh for n_samples
+    for _ in {1..$n_samples}; do
+        # ps -A all user processes, -o output cpu usage % and command path
+        # awk filter for cpu > cpu_thresh, then print only command path
+        ps -Ao %cpu,pid,comm \
+            | awk '$1 > '$cpu_thresh' { $1=""; sub(/^ /,""); print }'
+        sleep 1
+    done \
+        | sort | uniq -c \
+        | awk '$1 == '$n_samples' { $1=""; sub(/^ /,""); print }'
+    # sort to group same lines, uniq -c counts consecutive same lines
+    # awk filters for lines that occurred in every sample, then removes counts
+}
 
-if [[ "$cpu" -lt 90 ]]; then
-    sketchybar --set "$NAME" drawing=off
-    exit 0
+source "$CONFIG_DIR/helpers/hover.zsh"
+sketchy_args=(--set "$NAME")
+
+if [[ "$SENDER" == "routine" || "$SENDER" == "forced" ]]; then
+    procs="$(get_busy_processes)"
+    [[ -n "$procs" ]] \
+        && sketchy_args+=(drawing=on) \
+        || sketchy_args+=(drawing=off popup.drawing=off)
+
+    # clear cpu busy popup items
+    sketchy_args+=(--remove '/CPU_BUSY-\d*/')
+
+    # add item for each process
+    for proc in ${(f)procs}; do
+        pid="$(cut -w -f1 <<< "$proc")"
+        item_name="CPU_BUSY-$pid"
+
+        proc_name="$(cut -w -f2- <<< "$proc" | sed -E 's|.*/([^/]*)$|\1|')"
+        label="$proc_name ($pid)"
+
+        sketchy_args+=(
+            --add item "$item_name" "popup.$NAME"
+            --set "$item_name" label="$label" label.color=0xfffc897e
+        )
+    done
+else
+    $HOVERING \
+        && sketchy_args+=(popup.drawing=on) \
+        || sketchy_args+=(popup.drawing=off)
 fi
 
-# get process executable name at end of path
-proc="$(cut -w -f2- <<< "$ps_out" | sed -E 's|.*/([^/]*)$|\1|')"
-
-sketchybar --set "$NAME" drawing=on label="$proc"
+sketchybar "${sketchy_args[@]}"
