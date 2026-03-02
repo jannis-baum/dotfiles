96395600618b77a96a125912754340ab7d0fa232
Jannis Baum <plant.lull_1o@icloud.com>
2026-03-02 23:36:22 +0100

    zsh: adjust history options

--- config/zsh/scripts/options.zsh
+++ config/zsh/scripts/options.zsh
@@ -1,12 +1,17 @@
 setopt no_beep
 
 # history
+# increase number of history elements
+HISTSIZE=1000000
+SAVEHIST=$HISTSIZE
 # delete duplicates first when HISTFILE size exceeds HISTSIZE
 setopt hist_expire_dups_first
 # ignore duplicated commands history list
 setopt hist_ignore_dups
-# ignore commands that start with space
-setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+# remove superfluous blanks
+setopt hist_reduce_blanks
 # show command with history expansion to user before running it
 setopt hist_verify
 # live-append to history file, share items between sessions
