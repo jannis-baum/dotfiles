68c2da01d9621d5c1ab9ba2931cc82fb2ac919b6
Jannis Baum <plant.lull_1o@icloud.com>
2026-03-08 10:28:31 +0100

    sketchybar: basic now playing indicator

---
+++ config/sketchybar/items/now-playing/init.zsh
@@ -0,0 +1,41 @@
+item_dir="${0:a:h}"
+
+# if there are ever more items that need their own running process refactor
+# this into sketchybarrc so it takes care of all of these processes
+function _monitor_sketchybar() {
+    while [[ -n "$(sketchybar --query bar)" ]]; do
+        sleep 10
+    done
+
+    for proc in $@; do
+        kill $proc
+    done
+}
+
+function _update() {
+    while IFS= read -r line; do
+        local playing="$(jq -r .payload.playing <<< "$line")"
+        case "$playing" in
+            "null")
+                sketchybar --set now-playing drawing=off
+                continue
+                ;;
+            "true") local label_color="0xffbbbbbb";;
+            "false") local label_color="0xff808080";;
+        esac
+
+        local label="$(jq -r '"\(.payload.title) - \(.payload.artist)"' <<< "$line")"
+        sketchybar --set now-playing drawing=on label="$label" label.color="$label_color"
+    done
+}
+
+function _watch_media() {
+    media-control stream --no-diff | _update
+}
+
+sketchybar --add item now-playing right \
+           --set now-playing \
+               drawing=off
+
+_watch_media &!
+_monitor_sketchybar $! &!
